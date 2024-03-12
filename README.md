d09e615a127ca445c4a7d6ee45cc5691cc9b99ca
AddiSim <addisim12@gmail.com>
2024-03-12 11:50:17 +0000

    breytti i maven

---
+++ README.md
@@ -0,0 +1,2 @@
+# DayTripsD5
+This is a simble back end for an daytrip database
---
+++ model/Booked.java
@@ -0,0 +1,10 @@
+public class Book{
+    private Trip trip;
+
+    public Book(Trip trip){
+	this.trip = trip;
+    }
+
+    public Trip getBook(){
+	return trip;
+    }
